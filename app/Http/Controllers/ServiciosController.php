156a79d993df75d7c7c7d5abe50aad0d1134521b
Carlos Jiménez <carlosjimenez@192.168.68.150>
2022-11-29 15:31:07 -0600

    cambios

--- app/Http/Controllers/ServiciosController.php
+++ app/Http/Controllers/ServiciosController.php
@@ -28,7 +28,7 @@ class ServiciosController extends Controller
             $file = $request->file('filepond');
             $filename = $user->id . $user->name . "." . $file->guessExtension();
             $filePath = $file->storeAs('uploads/usuarios', $filename, 'public');
-            $user->user_image = $filePath;
+            $user->user_image = "storage/" . $filePath;
             $user->save();
             return $filePath;
         }
