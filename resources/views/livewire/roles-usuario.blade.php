273539762c079684940fcc4eedbc5264281e02ff
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-07 18:20:28 -0600

    Ignore self modal

--- resources/views/livewire/roles-usuario.blade.php
+++ resources/views/livewire/roles-usuario.blade.php
@@ -133,7 +133,7 @@
     </div>
 
     <!-- Modal View-->
-    <div class="modal fade" id="modalViewRol" tabindex="-1" role="dialog" aria-labelledby="modalViewRolLabel" aria-hidden="true">
+    <div wire:ignore.self class="modal fade" id="modalViewRol" tabindex="-1" role="dialog" aria-labelledby="modalViewRolLabel" aria-hidden="true">
         <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
