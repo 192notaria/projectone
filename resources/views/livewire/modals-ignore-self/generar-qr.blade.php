e3bcf19d02af7540831142f81e5cf9017ad81ed3
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-16 14:18:24 -0600

    Cambios qr

--- resources/views/livewire/modals-ignore-self/generar-qr.blade.php
+++ resources/views/livewire/modals-ignore-self/generar-qr.blade.php
@@ -9,7 +9,7 @@
             </div>
             <div class="modal-body">
                 <div class="visible-print text-center">
-                    <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}">
+                    {{-- <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}"> --}}
                     {{-- {!! QrCode::format('png')->size(400)->style('round')->generate("data"); !!} --}}
                 </div>
             </div>
