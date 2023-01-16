6ebc0f071038b1a7318e2a0b25f49444cbb12f07
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-16 14:48:13 -0600

    Creacion qr

--- resources/views/livewire/modals-ignore-self/generar-qr.blade.php
+++ resources/views/livewire/modals-ignore-self/generar-qr.blade.php
@@ -10,7 +10,7 @@
             <div class="modal-body">
                 <div class="visible-print text-center">
                     {{-- <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}"> --}}
-                    <img src="{!! QrCode::format('png')->size(400)->style('round')->generate("data"); !!}" alt="">
+                    <img src="{!! QrCode::format('png')->generate("data"); !!}" alt="">
                 </div>
             </div>
             <div class="modal-footer">
