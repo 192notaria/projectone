02d22814753675c0d2822fea064327882721093d
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-20 15:33:01 -0600

    icons

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -716,7 +716,7 @@
                         <div class="vistorsBrowser">
                             <div class="browser-list">
                                 <div class="w-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chrome"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="12" r="4"></circle><line x1="21.17" y1="8" x2="12" y2="8"></line><line x1="3.95" y1="6.06" x2="8.54" y2="14"></line><line x1="10.88" y1="21.94" x2="15.46" y2="14"></line></svg>
+                                    <i class="fa-solid fa-hard-drive text-white"></i>
                                 </div>
                                 <div class="w-browser-details">
                                     <div class="w-browser-info">
@@ -742,7 +742,7 @@
 
                             <div class="browser-list">
                                 <div class="w-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-compass"><circle cx="12" cy="12" r="10"></circle><polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon></svg>
+                                    <i class="fa-solid fa-memory text-white"></i>
                                 </div>
                                 <div class="w-browser-details">
 
