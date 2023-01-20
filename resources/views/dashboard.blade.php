eee67e80c2680d3da865a6dde860f1b85147d175
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-20 13:05:27 -0600

    Creacion del log del server

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,7 @@
     <link href="{{url('v3/src/assets/css/light/components/list-group.css')}}" rel="stylesheet" type="text/css">
     <link href="{{url('v3/src/assets/css/light/dashboard/dash_2.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{url('v3/src/assets/css/dark/dashboard/dash_2.css')}}" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="{{url('v3/src/assets/css/dark/widgets/modules-widgets.css')}}">
 
 @endsection
 @section('content')
@@ -15,7 +16,6 @@
     <div class="middle-content container-xxl p-0">
 
         <div class="row layout-top-spacing">
-
             <div class="col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                 <div class="widget widget-chart-one">
                     <div class="widget-heading">
@@ -701,6 +701,63 @@
 
                                 </tbody>
                             </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p>{{round(disk_total_space("/") / 1024 /1024 / 1024)}}</p>
+            <p>{{round(disk_free_space("/") / 1024 / 1024 / 1024)}}</p>
+            <p>{{((memory_get_usage() / 1024) / 1024)}}</p>
+            <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12 layout-spacing">
+                <div class="widget-four">
+                    <div class="widget-heading">
+                        <h5 class="">Server</h5>
+                    </div>
+                    <div class="widget-content">
+                        <div class="vistorsBrowser">
+                            <div class="browser-list">
+                                <div class="w-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chrome"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="12" r="4"></circle><line x1="21.17" y1="8" x2="12" y2="8"></line><line x1="3.95" y1="6.06" x2="8.54" y2="14"></line><line x1="10.88" y1="21.94" x2="15.46" y2="14"></line></svg>
+                                </div>
+                                <div class="w-browser-details">
+                                    <div class="w-browser-info">
+                                        <h6>HDD</h6>
+                                        @php
+                                            $totalhdd = round(disk_total_space("/") / 1024 /1024 / 1024);
+                                            $freeSpace = round(disk_free_space("/") / 1024 / 1024 / 1024);
+                                            $porcentje = $freeSpace * 100 / $totalhdd;
+                                            $ramusada = (memory_get_usage() / 1024) / 1024;
+                                            $totalRam = 16000;
+                                            $procentajeram = $ramusada * 100 / $totalRam;
+                                        @endphp
+                                        <p class="browser-count">{{$freeSpace}}GB Libres de {{$totalhdd}}GB | {{round($porcentje)}}% Usado</p>
+                                    </div>
+                                    <div class="w-browser-stats">
+                                        <div class="progress">
+                                            <div class="progress-bar bg-gradient-primary" role="progressbar" style="width: {{round($porcentje)}}%" aria-valuenow="{{round($porcentje)}}" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="browser-list">
+                                <div class="w-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-compass"><circle cx="12" cy="12" r="10"></circle><polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon></svg>
+                                </div>
+                                <div class="w-browser-details">
+
+                                    <div class="w-browser-info">
+                                        <h6>RAM</h6>
+                                        <p class="browser-count">{{round($ramusada,1)}}Mb usados de 16000Mb - {{round($procentajeram, 4)}}% Usado</p>
+                                    </div>
+
+                                    <div class="w-browser-stats">
+                                        <div class="progress">
+                                            <div class="progress-bar bg-gradient-danger" role="progressbar" style="width: {{round($procentajeram)}}%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
