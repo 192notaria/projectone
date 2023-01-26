d95ba69b283022bb3759e8c0cbc04d94a1a0fe43
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-26 15:13:46 -0600

    socket

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -27,6 +27,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
      key: 'MYKEY',
      wsHost: window.location.hostname,
      wsPort: 6001,
-     forceTLS: false,
-     disableStats: false
+     forceTLS: true,
+     disableStats: true
  });
