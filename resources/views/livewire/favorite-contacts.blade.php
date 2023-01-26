cd707fdeb51f4c5c16e6c00e8ac79f0dd2cdd441
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-26 15:10:47 -0600

    push

--- resources/views/livewire/favorite-contacts.blade.php
+++ resources/views/livewire/favorite-contacts.blade.php
@@ -104,7 +104,7 @@
             fd.append("user_id", userid);
 
             $.ajax({
-                url: "http://192.168.68.157/intefone",
+                url: "https://192.168.68.157/intefone",
                 type: 'POST',
                 data: fd,
                 success: function (data) {
