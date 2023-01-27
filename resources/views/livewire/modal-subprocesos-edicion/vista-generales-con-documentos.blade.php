486aeba646cd577152f08972cba8507f8ecc34a8
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-27 13:54:44 -0600

    ine

--- resources/views/livewire/modal-subprocesos-edicion/vista-generales-con-documentos.blade.php
+++ resources/views/livewire/modal-subprocesos-edicion/vista-generales-con-documentos.blade.php
@@ -80,9 +80,9 @@
                             <div class="col-lg-12 mt-3">
                                 <label for="">Identificacion oficial con fotografia</label>
                                 {{-- <embed src="{{url($generales_data->identificacion_oficial_con_foto)}}" style="width: 100%; height: 400px;" type="application/pdf"> --}}
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <iframe class="embed-responsive-item" src="{{url($generales_data->identificacion_oficial_con_foto)}}" style="width: 100%; height: 400px;" frameborder="0"></iframe>
-                                </div>
+                                <object data="{{url($generales_data->identificacion_oficial_con_foto)}}" type="application/pdf">
+                                    <embed src="{{url($generales_data->identificacion_oficial_con_foto)}}" type="application/pdf" />
+                                </object>
                             </div>
                         @endif
 
