fb9bc4183123c927349368f1b7ee7d385b8a6e4b
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-27 11:43:30 -0600

    jgj

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => '192.168.68.157',
+                'host' => env('APP_URL'),
                 'encrypted' => true,
                 'port' => 6001,
                 'scheme' => 'https',
