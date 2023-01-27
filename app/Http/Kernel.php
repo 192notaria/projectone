927b0cd79493573f7984fee00197e571a948fd8f
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-27 14:31:42 -0600

    cambio

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\LastUserActivity::class,
-            \App\Http\Middleware\FrameHeadersMiddleware::class
 
         ],
 
