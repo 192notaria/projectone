a3a2a0bb779409d045b61da4ea4be67f24b87b45
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-27 10:19:22 -0600

    pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,11 +37,16 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-                'useTLS' => true,
                 'host' => '127.0.0.1',
+                'encrypted' => true,
                 'port' => 6001,
-                'scheme' => 'http',
+                'scheme' => 'https',
+                'debug' => true,
+                'useTLS' => true,
+                'curl_options'=>[
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
             ],
         ],
 
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,15 +24,12 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
  window.Echo = new Echo({
      broadcaster: 'pusher',
-     key: 'MYKEY',
+     key: process.env.PUSHER_APP_KEY,
      wsHost: window.location.hostname,
-     encrypted: false,
      wsPort: 6001,
      wssPort: 6001,
+     encrypted: true,
      disableStats: true,
      forceTLS: true,
-     enabledTransports: ['ws','wss']
-
-     // wsPort: 6001, <-- Should these be commented out?
-     // wssPort: 6001, <-- Should these be commented out?
+     enabledTransports: ['ws','wss'],
  });
