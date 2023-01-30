93e0ac5c40a22f68d11cde60c992603cfcc9f73a
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-01-30 11:56:50 -0600

    asdaw

--- app/Http/Controllers/FirebaseAuthController.php
+++ app/Http/Controllers/FirebaseAuthController.php
@@ -63,8 +63,8 @@ class FirebaseAuthController extends Controller
                         "date" => $value->subproceso->created_at,
                     ];
                     array_push($arrayTemp[$value->proceso->nombre], $newdata);
-                    $arrayTemp[$value->proceso->nombre] = $arrayTemp[$key];
-                    unset($arrayTemp[$key]);
+                    $arrayTemp[$value->proceso->nombre] = $arrayTemp[$value->proceso->nombre];
+                    unset($arrayTemp[$value->proceso->nombre]);
                 }
 
                 $escrituraData = [
