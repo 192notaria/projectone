c123ffe1c35a7306bce61c04d338f2f0d91432d1
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-02-01 10:51:56 -0600

    sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -48,7 +48,7 @@
                 </ul>
             </li>
 
-            <li class="menu {{activeRoute(route('escrituras-proceso'))}} {{activeRoute(route('escrituras-apoyo'))}}">
+            <li class="menu {{activeRoute(route('escrituras-proceso'))}} {{activeRoute(route('escrituras-proceso2'))}} {{activeRoute(route('escrituras-apoyo'))}}">
                 <a href="#serviciosNav" data-bs-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
@@ -59,7 +59,7 @@
                     </div>
                 </a>
                 <ul class="collapse submenu list-unstyled" id="serviciosNav" data-bs-parent="#accordionExample">
-                    <li class="{{activeRoute(route('escrituras-proceso'))}}">
+                    <li class="{{activeRoute(route('escrituras-proceso'))}} {{activeRoute(route('escrituras-proceso2'))}}">
                         <a href="/administracion/escrituras_proceso"> Escrituras en proceso</a>
                     </li>
                     <li class="{{activeRoute(route('escrituras-apoyo'))}}">
