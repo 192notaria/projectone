b9b0f6a5276b0de27948c650a623dffc9ab3ef6c
Carlos Jiménez <carlosjimenez@192.168.68.137>
2023-02-02 16:04:08 -0600

    cambios

--- resources/views/livewire/guardias.blade.php
+++ resources/views/livewire/guardias.blade.php
@@ -13,7 +13,7 @@
     </div>
 
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div wire:ignore class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
