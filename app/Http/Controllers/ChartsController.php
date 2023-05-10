a323192c2548a81f2307da21aadd17f7b8694ed4
Carlos Jiménez <carlosjimenez@192.168.1.37>
2023-05-10 14:19:05 -0600

    cambios

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Proyectos;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -11,23 +12,50 @@ class ChartsController extends Controller
     public function index(Request $request){
 
     if($request->type == 'area'){
-        $data = [
-            ["x" => "Enero", "y" => "120000"],
-            ["x" => "Febrero", "y" => "190000"],
-            ["x" => "Marzo", "y" => "12000"],
-            ["x" => "Abril", "y" => "120000"],
-            ["x" => "Mayo", "y" => "13900"],
-            ["x" => "Junio", "y" => "190000"],
-            ["x" => "Julio", "y" => "12200"],
-            ["x" => "Agosto", "y" => "120000"],
-            ["x" => "Septiembre", "y" => "13200"],
-            ["x" => "Octubre", "y" => "15000"],
-            ["x" => "Noviembre", "y" => "220000"],
-            ["x" => "Diciembre", "y" => "1200"],
-        ];
+        $proyectos = Proyectos::select("id", "created_at")->get()->groupBy(function ($date){
+            return Carbon::parse($date->created_at)->format('m'); // grouping by months
+        });
+
+        $mes = "";
+        $proyectosArray = [];
+        foreach ($proyectos as $key => $value) {
+
+            if((int)$key == 1) $mes = "Enero";
+            if((int)$key == 2) $mes = "Febrero";
+            if((int)$key == 3) $mes = "Marzo";
+            if((int)$key == 4) $mes = "Abril";
+            if((int)$key == 5) $mes = "Mayo";
+            if((int)$key == 6) $mes = "Junio";
+            if((int)$key == 7) $mes = "Julio";
+            if((int)$key == 8) $mes = "Agosto";
+            if((int)$key == 9) $mes = "Septiembre";
+            if((int)$key == 10) $mes = "Octubre";
+            if((int)$key == 11) $mes = "Noviembre";
+            if((int)$key == 12) $mes = "Diciembre";
+
+            $data = ["x" => $mes, "y" => count($value)];
+            array_push($proyectosArray, $data);
+            // $proyectosArray[(int)$key] = count($value);
+        }
+        return json_encode($proyectosArray);
+
+        // $data = [
+        //     ["x" => "Enero", "y" => "120000"],
+        //     ["x" => "Febrero", "y" => "190000"],
+        //     ["x" => "Marzo", "y" => "12000"],
+        //     ["x" => "Abril", "y" => "120000"],
+        //     ["x" => "Mayo", "y" => "13900"],
+        //     ["x" => "Junio", "y" => "190000"],
+        //     ["x" => "Julio", "y" => "12200"],
+        //     ["x" => "Agosto", "y" => "120000"],
+        //     ["x" => "Septiembre", "y" => "13200"],
+        //     ["x" => "Octubre", "y" => "15000"],
+        //     ["x" => "Noviembre", "y" => "220000"],
+        //     ["x" => "Diciembre", "y" => "1200"],
+        // ];
 
         $datareturn = [
-            "data" => $data,
+            "data" => $proyectosArray,
             "type" => $request->type
         ];
 
