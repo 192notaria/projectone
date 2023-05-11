d0db644934abe26326be5fb1a4a5f552ebc2829f
Carlos Jiménez <carlosjimenez@192.168.1.18>
2023-05-11 11:08:52 -0600

    cambios

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
             <div class="col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                 <div class="widget widget-chart-one">
                     <div class="widget-heading">
-                        <h5 class="">Ingresos Anuales</h5>
+                        <h5 class="">Proyectos anuales</h5>
                         <div class="task-action">
                             <div class="dropdown">
                                 <a class="dropdown-toggle" href="#" role="button" id="renvenue" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
