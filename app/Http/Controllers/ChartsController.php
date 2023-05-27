13d8777a94aeda2354590d3cbc45669cfb0e92fe
Carlos Jiménez <carlosjimenez@192.168.1.34>
2023-05-26 19:56:38 -0600

    cambios

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -6,6 +6,7 @@ use App\Models\Proyectos;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Kreait\Firebase\Factory;
 
 class ChartsController extends Controller
 {
