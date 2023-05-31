68b4c22c6d96fe8bb518e45783dfee73cf64bb65
Carlos Jiménez <carlosjimenez@192.168.1.46>
2023-05-31 11:17:42 -0600

    cambios

--- resources/views/livewire/numeros-escrituas-guardados.blade.php
+++ resources/views/livewire/numeros-escrituas-guardados.blade.php
@@ -81,10 +81,10 @@
                         @forelse ($escrituras as $escritura)
                             <tr>
                                 <td>{{$escritura->numero_escritura}}</td>
-                                <td>{{$escritura->volumen ?? "S/V"}}</td>
-                                <td>{{$escritura->abogado->name ?? "Sin abogado asignado"}}</td>
-                                <td>{{$escritura->folio_inicio ?? "S/F"}} - {{$escritura->folio_fin ?? "S/F"}}</td>
-                                <td>{{$escritura->created_at ?? "S/F"}}</td>
+                                <td>{!! $escritura->volumen ?? "<span class='text-danger'>S/V</span>" !!}</td>
+                                <td>{!! $escritura->abogado->name ?? "<span class='text-danger'>Sin abogado asignado</span>" !!}</td>
+                                <td>{!! $escritura->folio_inicio ?? "<span class='text-danger'>S/F</span>" !!} - {!!$escritura->folio_fin ?? "<span class='text-danger'>S/F</span>"!!}</td>
+                                <td>{!! $escritura->created_at ?? "<span class='text-danger'>S/F</span>" !!}</td>
                                 <td>
                                     <button class="btn btn-outline-primary" wire:click='editarNumero({{$escritura->id}})'>
                                         <i class="fa-solid fa-pen-to-square"></i>
