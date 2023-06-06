9d161ab2011fc6e75ee205400b598c33566781b1
Carlos Jiménez <carlosjimenez@192.168.1.61>
2023-06-06 15:30:44 -0600

    cambios

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\UsuariosController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::get('/dashboard', function () {
