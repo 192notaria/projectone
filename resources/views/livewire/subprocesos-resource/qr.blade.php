6a6808cbfd6f362ec99794407630c292041429b8
Carlos Jiménez <carlosjimenez@192.168.1.26>
2023-06-07 10:37:42 -0600

    logo

--- resources/views/livewire/subprocesos-resource/qr.blade.php
+++ resources/views/livewire/subprocesos-resource/qr.blade.php
@@ -3,7 +3,7 @@
         <div class="visible-print text-center">
             {{-- <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}"> --}}
             @if ($proyecto_activo)
-                <img style="width: 500px; height: 500px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/rounded-logo-notaria.png"), .3, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
+                <img style="width: 500px; height: 500px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/notaria192logo2.png"), .3, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
             @endif
         </div>
     </div>
