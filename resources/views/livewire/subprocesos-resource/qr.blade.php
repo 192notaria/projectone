f204f3f03b0e0c834df6d5c237b568a019af5217
Carlos Jiménez <carlosjimenez@192.168.1.41>
2023-06-07 11:07:51 -0600

    cambios

--- resources/views/livewire/subprocesos-resource/qr.blade.php
+++ resources/views/livewire/subprocesos-resource/qr.blade.php
@@ -1,10 +1,28 @@
 <div class="row">
-    <div class="col-lg-12">
-        <div class="visible-print text-center">
-            {{-- <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}"> --}}
-            @if ($proyecto_activo)
+    @if ($proyecto_activo)
+        <div class="col-lg-6">
+            <div class="visible-print text-center">
+                {{-- <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}"> --}}
                 <img style="width: 500px; height: 500px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/192logocoloreado.png"), .4, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
-            @endif
+            </div>
         </div>
-    </div>
+        <div class="col-lg-6">
+            <div class="visible-print text-center">
+                {{-- <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}"> --}}
+                <img style="width: 500px; height: 500px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/rounded-logo-notaria-coloreado.png"), .4, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="visible-print text-center">
+                {{-- <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}"> --}}
+                <img style="width: 500px; height: 500px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/notarialogocoloreado.png"), .4, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div class="visible-print text-center">
+                {{-- <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}"> --}}
+                <img style="width: 500px; height: 500px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/rounded-logo-notaria.png"), .4, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
+            </div>
+        </div>
+    @endif
 </div>
