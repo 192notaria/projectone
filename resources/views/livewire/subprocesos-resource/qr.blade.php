af52a61fa192f1261ea0c78a935caa2f3cac22ba
Carlos Jiménez <carlosjimenez@192.168.1.41>
2023-06-07 11:11:04 -0600

    cambios

--- resources/views/livewire/subprocesos-resource/qr.blade.php
+++ resources/views/livewire/subprocesos-resource/qr.blade.php
@@ -15,7 +15,7 @@
         <div class="col-lg-6">
             <div class="visible-print text-center">
                 {{-- <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}"> --}}
-                <img style="width: 500px; height: 500px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/notarialogocoloreado.png"), .4, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
+                <img style="width: 500px; height: 500px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/notarialogocoloreado.png"), 1, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
             </div>
         </div>
         <div class="col-lg-6">
