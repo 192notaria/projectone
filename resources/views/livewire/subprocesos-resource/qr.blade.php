43410e36f11d72e5993bc189a4931fc57e7e57d9
Carlos Jiménez <carlosjimenez@192.168.1.26>
2023-06-07 10:28:47 -0600

    cambios

--- resources/views/livewire/subprocesos-resource/qr.blade.php
+++ resources/views/livewire/subprocesos-resource/qr.blade.php
@@ -3,7 +3,7 @@
         <div class="visible-print text-center">
             {{-- <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}"> --}}
             @if ($proyecto_activo)
-                <img style="width: 200px; height: 200px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge('https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png', .3, true)->size(100)->style('round')->generate($proyecto_activo->qr)) !!} ">
+                <img style="width: 500px; height: 500px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge('https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png', .3, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
             @endif
         </div>
     </div>
