5516a5717b287e39fac8b108128bf66c23a75e67
Carlos Jiménez <carlosjimenez@192.168.1.26>
2023-06-07 10:07:13 -0600

    cambios

--- resources/views/livewire/modals-ignore-self/generar-qr.blade.php
+++ resources/views/livewire/modals-ignore-self/generar-qr.blade.php
@@ -11,7 +11,7 @@
                 <div class="visible-print text-center">
                     {{-- <img src="{!!QrCode::format('png')->generate('Embed me into an e-mail!'), 'QrCode.png', 'image/png'!!}"> --}}
                     @if ($qrData)
-                        <img style="width: 200px; height: 200px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->size(100)->style('round')->generate($qrData)) !!} ">
+                        <img style="width: 200px; height: 200px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge('https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png', .3, true)->size(100)->style('round')->generate($qrData)) !!} ">
                     @endif
                 </div>
             </div>
