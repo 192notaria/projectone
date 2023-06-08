f4e6df64f17b0c834f240333e2e42d36bbc12bea
Carlos Jiménez <carlosjimenez@192.168.1.24>
2023-06-08 11:51:04 -0600

    cambios

--- resources/views/livewire/subprocesos-resource/qr.blade.php
+++ resources/views/livewire/subprocesos-resource/qr.blade.php
@@ -1,8 +1,8 @@
 <div class="row gy-3 gx-3">
     @if ($proyecto_activo)
-        <div class="col-lg-6">
+        <div class="col-lg-12">
             <div class="visible-print text-center">
-                <img style="width: 200px; height: 200px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/notarialogocoloreado.png"), 1, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
+                <img style="width: 300px; height: 300px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/notarialogocoloreado.png"), 1, true)->size(400)->style('round')->generate($proyecto_activo->qr)) !!} ">
             </div>
         </div>
     @endif
