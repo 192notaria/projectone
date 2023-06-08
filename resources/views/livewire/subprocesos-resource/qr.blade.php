5eb885d6f7b1abf7b2f59ff23c2ed42d00371760
Carlos Jiménez <carlosjimenez@192.168.1.24>
2023-06-08 11:50:10 -0600

    cambios

--- resources/views/livewire/subprocesos-resource/qr.blade.php
+++ resources/views/livewire/subprocesos-resource/qr.blade.php
@@ -1,24 +1,9 @@
-{{-- <div class="row gy-3 gx-3">
+<div class="row gy-3 gx-3">
     @if ($proyecto_activo)
-        <div class="col-lg-3">
-            <div class="visible-print text-center">
-                <img style="width: 200px; height: 200px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/192logocoloreado.png"), .5, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
-            </div>
-        </div>
-        <div class="col-lg-3">
-            <div class="visible-print text-center">
-                <img style="width: 200px; height: 200px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/rounded-logo-notaria-coloreado.png"), .5, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
-            </div>
-        </div>
-        <div class="col-lg-3">
+        <div class="col-lg-6">
             <div class="visible-print text-center">
                 <img style="width: 200px; height: 200px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/notarialogocoloreado.png"), 1, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
             </div>
         </div>
-        <div class="col-lg-3">
-            <div class="visible-print text-center">
-                <img style="width: 200px; height: 200px;" src="data:image/png;base64, {!! base64_encode(QrCode::size(400)->format('png')->merge(url("v3/src/assets/img/rounded-logo-notaria.png"), .5, true)->size(300)->style('round')->generate($proyecto_activo->qr)) !!} ">
-            </div>
-        </div>
     @endif
-</div> --}}
+</div>
