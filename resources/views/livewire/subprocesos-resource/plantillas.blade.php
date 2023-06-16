f21ed481cb1bb079b3459812038552de1f3613ad
Carlos Jiménez <carlosjimenez@192.168.1.44>
2023-06-16 09:45:04 -0600

    cambios

--- resources/views/livewire/subprocesos-resource/plantillas.blade.php
+++ resources/views/livewire/subprocesos-resource/plantillas.blade.php
@@ -1,8 +1,8 @@
 
 <h1>Plantillas</h1>
 <div class="row gx-3 gy-3">
-    <div class="col-lg-12 tble-responsive">
-        <table class="table">
+    <div class="col-lg-12 table-responsive">
+        <table class="table table-responsive">
             <thead>
                 <tr>Nombre</tr>
                 <tr>Descripcion</tr>
