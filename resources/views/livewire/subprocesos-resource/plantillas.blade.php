76b2e2a972d1087d4b8521aef86df1c2378ff098
Carlos Jiménez <carlosjimenez@192.168.1.44>
2023-06-16 09:44:07 -0600

    cambios

--- resources/views/livewire/subprocesos-resource/plantillas.blade.php
+++ resources/views/livewire/subprocesos-resource/plantillas.blade.php
@@ -3,7 +3,14 @@
 <div class="row gx-3 gy-3">
     <div class="col-lg-12 tble-responsive">
         <table class="table">
-            <thead></thead>
+            <thead>
+                <tr>Nombre</tr>
+                <tr>Descripcion</tr>
+                <tr>Descargar</tr>
+            </thead>
+            <tbody>
+                <td class="text-center" colspan="3">Sin registros</td>
+            </tbody>
         </table>
     </div>
 </div>
