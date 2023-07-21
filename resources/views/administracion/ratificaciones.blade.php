26cb0c68656830468fd54926b60774398de2d98d
Carlos Jiménez <carlosjimenez@192.168.1.58>
2023-07-20 19:40:02 -0600

    cambios

--- resources/views/administracion/ratificaciones.blade.php
+++ resources/views/administracion/ratificaciones.blade.php
@@ -25,11 +25,6 @@
 @section('content')
     <div class="layout-px-spacing">
         <div class="middle-content container-xxl p-0">
-            <div class="page-meta">
-                <nav class="breadcrumb-style-one" aria-label="breadcrumb">
-                    <h2>Ratificaciones</h2>
-                </nav>
-            </div>
             <div class="row layout-top-spacing">
                 <div class="col-lg-12">
                     @livewire("ratificaciones")
--- resources/views/livewire/ratificaciones.blade.php
+++ resources/views/livewire/ratificaciones.blade.php
@@ -20,7 +20,7 @@
 
             @can("crear-proyectos")
                 <button wire:click='modalNuevoProyecto' wire:loading.attr='disabled' type="button" class="btn btn-outline-dark">
-                    Nuevo proyecto <i class="fa-solid fa-user-plus"></i>
+                    Nueva ratificación <i class="fa-solid fa-user-plus"></i>
                 </button>
             @endcan
         </div>
