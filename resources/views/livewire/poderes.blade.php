261f62504c0916ce2f09ee719eba24944ca0ee50
Carlos Jiménez <carlosjimenez@192.168.1.58>
2023-07-21 19:09:34 -0600

    cambios

--- resources/views/livewire/poderes.blade.php
+++ resources/views/livewire/poderes.blade.php
@@ -20,7 +20,7 @@
                         <option value="10">10</option>
                         <option value="15">15</option>
                         <option value="20">20</option>
-                       22 <option value="50">50</option>
+                        <option value="50">50</option>
                     </select>
                 </div>
                 <div>
