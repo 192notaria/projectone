76ac728a4be3fff50000c8c320959794d2394a19
Carlos Jiménez <carlosjimenez@192.168.1.46>
2023-08-07 14:55:50 -0600

    cambios

--- resources/views/administracion/servicios.blade.php
+++ resources/views/administracion/servicios.blade.php
@@ -1,14 +1,11 @@
 @extends('layouts.app')
 @section('links-content')
-    {{-- <link href="{{ url('v3/src/assets/css/light/elements/search.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ url('v3/src/assets/css/dark/elements/search.css') }}" rel="stylesheet" type="text/css" /> --}}
-
     <link href="{{ url('v3/src/assets/css/light/forms/switches.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ url('v3/src/assets/css/dark/forms/switches.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{url('v3/src/assets/css/dark/users/user-profile.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{url('v3/src/assets/css/light/elements/custom-pagination.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{url('v3/src/assets/css/dark/elements/custom-pagination.css')}}" rel="stylesheet" type="text/css" />
-    {{-- <link href="{{url('filepond-master/dist/filepond.css')}}" rel="stylesheet" type="text/css" /> --}}
+
     <link href="{{url('v3/src/assets/css/dark/components/tabs.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{url('v3/src/assets/css/light/components/tabs.css')}}" rel="stylesheet" type="text/css" />
 @endsection
@@ -30,23 +27,6 @@
 @endsection
 
 @section('scripts-content')
-    {{-- <script src="{{ url("filepond-master/dist/filepond.js") }}"></script> --}}
     <script src="{{ url("v3/src/plugins/src/highlight/highlight.pack.js") }}"></script>
-    {{-- <script src="{{ url('v3/src/assets/js/elements/custom-search.js') }}"></script> --}}
-
-    {{-- <script>
-        const inputElement = document.querySelector('input[type="file"]');
-        const token = document.querySelector('input[name="_token"]');
-        const pond = FilePond.create(inputElement);
-        FilePond.setOptions({
-            labelIdle: "Arrastra y suelta aqui o... <a class='btn btn-primary'>Buscar...</a>",
-            server: {
-                url: "/administracion/servicios/uploadFile",
-                headers: {
-                    'X-CSRF-TOKEN': token.value
-                }
-            }
-        })
-    </script> --}}
 @endsection
 
